220d48afa122e223579f9208b094ae8ed83f3c1d
manudiv16 <38869988+manudiv16@users.noreply.github.com>
2024-06-24 00:19:50 +0200

    remove loom project

--- loom/src/main/scala/saft/Logging.scala
+++
@@ -1,8 +0,0 @@
-package saft
-
-import org.slf4j.MDC
-
-object Logging {
-  def setNodeLogAnnotation(nodeId: NodeId): Unit = MDC.put("nodeId", "node" + nodeId.number)
-  def setRoleLogAnnotation(state: String): Unit = MDC.put("role", state)
-}
--- loom/src/main/scala/saft/ServerEvent.scala
+++
@@ -1,9 +0,0 @@
-package saft
-
-enum ServerEvent:
-  /** Used when an election or heartbeat timeout occurs. */
-  case Timeout extends ServerEvent
-
-  /** @param respond Sends a response back to the server or client that sent this request. */
-  case RequestReceived(message: RequestMessage, respond: ResponseMessage => Unit) extends ServerEvent
-  case ResponseReceived(message: ResponseMessage with ToServerMessage) extends ServerEvent
